9404cdca8be764f8b2df1802c8e15cb856af7885
Jeremiah Martin <urlimitlessaf@gmail.com>
2022-07-25 17:37:57 -0600

    Project update 2

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -9,31 +9,22 @@ export const projects = [
     id: 0,
   },
   {
-    title: 'React AI Website',
-    description:"During completetion of this project I was introduced to React functional components and their reusability, React file and folder structure, Fundamentals of the CSS BEM Model, and other react fundamentals.",
-    image: '/images/2.png',
-    tags: ['React', 'JavaScript', 'CSS'],
-    source: 'https://github.com/LimitlessUR/React_AI_Website',
-    visit: 'https://github.com/LimitlessUR/React_AI_Website',
+    title: 'Live React UI/UX Restuarant Website',
+    description:"During this project I was introduced to the fundementals of UI/UX design, React file and folder structure for a highly aesthetic website, and how to form media queries for satisfactory responsiveness covering almost all devices ",
+    image: '/images/6.png',
+    tags: ['ReactJS', 'UI/UX', 'CSS', 'Visit via Source button'],
+    source: 'https://master.d1qddam9popti2.amplifyapp.com',
+    visit: 'https://github.com/LimitlessUR/Restaurant_UI-UX_Website',
     id: 1,
   },
   {
-    title: 'React Exercise App APIs',
+    title: 'Live React Exercise App APIs',
     description: "Given my experience in the fitness industry, this app was a lot of fun to program. Here, our user can search through the exercisedb API for over 1300 different exercises to add to thier routine. Try it out yourself",
     image: '/images/5.png',
-    tags: ['Reactjs', 'API', 'Deployed'],
-    source: 'https://github.com/LimitlessUR/Real_Estate_nextjsAPI',
-    visit: 'https://dev5303.d2s0x0njky1gl5.amplifyapp.com/',
+    tags: ['Reactjs', 'API', 'CSS', 'Visit via Source button'],
+    source: 'https://dev5303.d2s0x0njky1gl5.amplifyapp.com/',
+    visit: 'https://github.com/LimitlessUR/ReactJS_Exercise_App_APIs',
     id: 3,
-  },
-  {
-    title: 'Django Todo list',
-    description: "Here we have a simple Django Todo list application that enables users to add to a list as well as delete from the list. Here I learned the fundametnals of Django framework and got to utilized some simple Python logic to build this fun, easy to use application.",
-      image: '/images/3.jpg',
-      tags: ['Django', 'Python', 'HTML'],
-    source: 'https://github.com/LimitlessUR/django-todo',
-    visit: 'https://github.com/LimitlessUR/django-todo',
-    id: 2,
   },
   {
     title: 'Space Invaders',
@@ -44,6 +35,27 @@ export const projects = [
     visit: 'https://github.com/LimitlessUR/Python_SpaceShooter',
     id: 3,
   },
+  
+  
+  {
+    title: 'Django Todo list',
+    description: "Here we have a simple Django Todo list application that enables our user to add to a list as well as delete from the list. Here I learned the fundametnals of Django framework and got to utilized some simple Python logic to build this fun, easy to use application.",
+      image: '/images/3.jpg',
+      tags: ['Django', 'Python', 'HTML'],
+    source: 'https://github.com/LimitlessUR/django-todo',
+    visit: 'https://github.com/LimitlessUR/django-todo',
+    id: 2,
+  },
+  
+  {
+    title: 'Live React AI Website',
+    description:"During completetion of this project I was introduced to React functional components and their reusability, React file and folder structure, Fundamentals of the CSS BEM Model, and other react fundamentals.",
+    image: '/images/2.png',
+    tags: ['React', 'JavaScript', 'CSS', 'Visit via Source button' ],
+    source: 'https://dev1734.d2m9mq0vf8kq5e.amplifyapp.com',
+    visit: 'https://github.com/LimitlessUR/React_AI_Website',
+    id: 1,
+  },
   {
     title: 'Currently working on the MERN Stack, and Reactjs Projects',
     description: "So many fun projects, so little time! Check out my other projects throught the Code link below",
@@ -53,7 +65,6 @@ export const projects = [
     visit: 'https://github.com/LimitlessUR',
     id: 3,
   },
-  
 ];
 
 export const TimeLineData = [
